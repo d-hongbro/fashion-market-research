5106c824fc90028bcc92087bf75b490b5551c444
David Hong <davidhong342@gmail.com>
2017-10-12 13:10:18 -0700

    testing ajax call to amazon api

--- js/app.js
+++ js/app.js
@@ -4,8 +4,9 @@ const AWS_SK = 'rUCqEqtC6p0ikg+14kC1QVOWflYtY6G4GwlNelJJ';
 const AWS_ENDPOINT = 'webservices.amazon.com';
 const AWS_URI = '/onca/xml';
 
-/*  keys created in IAM  */
-// const AWS_AT = 'ProductAdvertisingAPI-user';
+
+  // keys created in IAM  
+// const AWS_AT = 'hongbro-20';
 // const AWS_AK = 'AKIAJBP5ORL5VRQBNHXA';
 // const AWS_SK = 'TMN0tUxrIJLhJbu2J5TCYmkXK+ovD5DWsqVOAUzN';
 // const AWS_ENDPOINT = 'webservices.amazon.com';
@@ -197,9 +198,78 @@ function callBack(data) {
 function getAmazonDataFromApiTwo(endpoint, callBack) {
 	// let xhr = createCORSRequest('GET', endpoint);
 	// xhr.send();
-	console.log('running');
-	console.log($.getJSON(endpoint, callBack));
+
+	$.ajax({
+		url: endpoint,
+		type: 'GET',
+		crossDomain: true,
+		dataType: 'jsonp'
+		}).fail(function(jqXHR, error){ debugger; //this waits for the ajax to return with an error promise object
+			console.log(`error: ${error}`);
+			console.log(`jqXHR: ${jqXHR}`);
+		});
+
+	// console.log('running');
+	// $.getJSON(endpoint)
+	// 	.always(function( jqXHR, textStatus, errorThrown ) {
+	// 		console.log(jqXHR.responseText);
+	// 	});
+
+		console.log(endpoint);
+	}
+
+
+function getAmazonDataFromApiThree(query, signature, canonicalString) {
+	let newQuery = {};
+	const url = `https://${AWS_ENDPOINT}${AWS_URI}`;
+	query.map(item => {
+		const key = Object.keys(item);
+		const value = item[key];
+		newQuery[key] = value;
+	});
+
+	newQuery['Signature'] = signature;
+	canonicalString += `&Signature=${signature}`;
+	console.log(newQuery);
+	 $.ajax({
+		url: url,
+		// data: JSON.stringify(newQuery),
+		data: canonicalString,
+		dataType: "json",
+		type: "GET",
+		crossDomain: true,
+		contentType: 'application/json'
+		}).fail(function(jqXHR, error){ debugger; //this waits for the ajax to return with an error promise object
+			console.log(`error: ${error}`);
+			console.log(`jqXHR: ${jqXHR}`);
+		});
 }
+
+function jsonCallback(json){
+  console.log(json);
+}
+
+	function getShows(item){
+	
+		// the parameters we need to pass in our request to Bandsintown's API
+		var request = { 
+			location: getLocation(),
+			app_id: 'supp',
+			api_version: '2.0',
+			order: 'asc',
+			sort: 'datetime',
+			limit: 75
+		};
+		
+		 return $.ajax({
+			url: "https://api.bandsintown.com/events/search",
+			data: request,
+			dataType: "jsonp",
+			type: "GET"
+		}).fail(function(jqXHR, error){ debugger; //this waits for the ajax to return with an error promise object
+			nothingToShow();
+		});
+	};
 
 function getAmazonQueryTwo(searchTerm, timestamp) {
 	return {
@@ -250,16 +320,30 @@ function getAmazonRequestUrlTwo(signature, canonicalString) {
 
 function handleAmazonApiTwo() {
 	const timestamp = getCurrentTimeStamp();
+	// console.log(timestamp);
+	// const timestamp = '2017-10-11T19:54:50.000Z';
+	// const timestamp = '2017-10-11T19%3A53%3A06.000Z';
 	const query = getAmazonQueryTwo("crop", timestamp);
 	const sortedQuery = sortByByteSizeTwo(query);
+	console.log(sortedQuery);
 	const canonicalString = getCanonicalStringTwo(sortedQuery);
 	const signedString = prepareSignStringTwo(canonicalString);
 	const signature = getAmazonSignatureTwo(signedString);
 	const requestUrl = getAmazonRequestUrlTwo(signature, canonicalString);
-	getAmazonDataFromApiTwo(requestUrl, callBack);
+	//getAmazonDataFromApiTwo(requestUrl, callBack);
+	getAmazonDataFromApiThree(sortedQuery,signature, canonicalString);
 }
 
 $(handleAmazonApiTwo);
 
+//http://webservices.amazon.com/onca/xml?AWSAccessKeyId=AKIAJUTQBV5ZSIZVARLA&AssociateTag=hongbro-20&Keywords=crop&Operation=ItemSearch&SearchIndex=All&Service=AWSECommerceService&Timestamp=2017-10-12T19%3A48%3A35.000Z&Signature=Dop83Iz4Jcyoex%2Bgm5pDcL8q0Jj6fZAlMpNEk%2FMbgmk%3D
+//https://webservices.amazon.com/onca/xml?AWSAccessKeyId=AKIAJUTQBV5ZSIZVARLA&AssociateTag=hongbro-20&Keywords=crop&Operation=ItemSearch&SearchIndex=All&Service=AWSECommerceService&Timestamp=2017-10-11T19%3A33%3A06.000Z&Signature=yXuu21UFtu03ORlA07GP4SKlgwEfOYvKMFNqgXdAheA%3D
 //http://webservices.amazon.com/onca/xml?AWSAccessKeyId=AKIAJUTQBV5ZSIZVARLA&AssociateTag=hongbro-20&Keywords=crop&Operation=ItemSearch&SearchIndex=All&Service=AWSECommerceService&Timestamp=2017-10-11T08%3A11%3A44.000Z&Signature=sKyBxOMQa%2BKOll13TiltYPhgvadghiI0vLUqwTdo5Fo%3D
 //http://webservices.amazon.com/onca/xml?AWSAccessKeyId=AKIAJUTQBV5ZSIZVARLA&AssociateTag=hongbro-20&Keywords=crop&Operation=ItemSearch&SearchIndex=All&Service=AWSECommerceService&Timestamp=2017-10-11T08%3A11%3A41.000Z&Signature=QJeGMpkT0lXNjSvzLM%2BZSvbl5fq0zK1ZSOmEg4SP6sI%3D
+//http://webservices.amazon.com/onca/xml?AWSAccessKeyId=AKIAJUTQBV5ZSIZVARLA&AssociateTag=hongbro-20&Keywords=crop&Operation=ItemSearch&SearchIndex=All&Service=AWSECommerceService&Timestamp=2017-10-12T19%3A48%3A35.000Z&Signature=Dop83Iz4Jcyoex%2Bgm5pDcL8q0Jj6fZAlMpNEk%2FMbgmk%3D
+// 2017-10-12T19%3A50%3A48.000Z
+// 2017-10-12T19%3A48%3A35.000Z
+
+
+//https://webservices.amazon.com/onca/xml?AWSAccessKeyId=AKIAJUTQBV5ZSIZVARLA&AssociateTag=hongbro-20&Keywords=crop&Operation=ItemSearch&SearchIndex=All&Service=AWSECommerceService&Timestamp=2017-10-12T20%3A02%3A45.000Z&Signature=3%2B5kUa3imZC6oceQn4qtWxep1G7wlHFpHj6n%2B1JKcbA%3D
+//https://webservices.amazon.com/onca/xml?AWSAccessKeyId=AKIAJUTQBV5ZSIZVARLA&AssociateTag=hongbro-20&Keywords=crop&Operation=ItemSearch&SearchIndex=All&Service=AWSECommerceService&Timestamp=2017-10-12T20%3A02%3A45.000Z&Signature=3%2B5kUa3imZC6oceQn4qtWxep1G7wlHFpHj6n%2B1JKcbA%3D
